cbeeee817b9bc70481a54ed2c377abaf4bdc3894
PRAKHAR DHYANI <prakhardhyani1000@gmail.com>
2021-06-17 06:25:32 +0530

    updated js

--- app.js
+++ app.js
@@ -1,39 +1,71 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   var instr = document.querySelector(".instructions");
   var btn = document.querySelector(".btn");
+  var signOut = document.querySelector(".signOut");
   var text = document.querySelector(".text");
   var count = 0;
   var userName = document.getElementById("name");
   var score = 0;
   //checking if user name exist or not
-
-  if (localStorage.name !== undefined) {
-    localStorage.removeItem("clickcount");
+  function Userexist() {
+    if (localStorage.name) {
+      document.getElementById("nameHead").innerHTML =
+      "Hi " + localStorage.name + " Welcome Back..! ";
+      document.getElementById("name").style.display = "none";
+      document.getElementById("not").innerHTML =
+      " Not " +
+      localStorage.name +
+      " Click Below to sign out ( Note : Your Best Score will got reset) ";
+    }
   }
+  //checking bestscore
+  function bestScore() {
+    if (localStorage.bestscore) {
+      document.getElementsByClassName("text").innerHTML =
+      "Your Best Score is " +
+      localStorage.bestscore +
+      " Play now to beat it ✌🤞😉😎  ";
+    }
+  }
+  Userexist();
+  // bestScore();
   // storing the user name in Local Storage
   userName.onchange = function () {
     userName = userName.value;
+    // alert(userName);
+    //displaying signout button
     localStorage.setItem("name", userName);
     localStorage.setItem("bestscore", score);
-    if (localStorage.name != undefined) {
-      document.getElementById("nameHead").innerHTML =
-        "Hi " + localStorage.name + " Welcome Back..! ";
-      document.getElementById("name").style.display = "none";
-    }
+    signOut.style.display = "inline-block";
+
+    //checking if user name exist or not
+    Userexist();
     //storing best score in the localStorage
-    if (localStorage.bestscore !== undefined) {
-      document.getElementsByClassName("text").innerHTML =
-        "Your Best Score is " +
-        localStorage.bestscore +
-        " Play now to beat it ✌🤞😉😎  ";
-    }
-    // alert(localStorage.name);
+    // bestscore();
+
   };
   //onclicking button instructions will apeared and start button will appeared
   btn.addEventListener("click", function () {
     instr.style.display = "inline-block";
     btn.style.display = "none";
-    // localStorage.removeItem("name");
+    if (localStorage.name) {
+      document.getElementById("nameHead").innerHTML =
+        "Hi " + localStorage.name + " Welcome Back..! ";
+      document.getElementById("name").style.display = "none";
+      document.getElementById("not").innerHTML =
+        " Not " +
+        localStorage.name +
+        " Click Below to sign out ( Note : Your Best Score will got reset) ";
+    }
+  });
+  //onclicking signout button
+  signOut.addEventListener("click", function () {
+    signOut.style.display = "none";
+    localStorage.removeItem("name");
+    document.getElementById("nameHead").innerHTML = "Hi User Welcome Back..! ";
+    document.getElementById("name").style.display = "inline-block";
+    document.getElementById("not").style.display = "none";
   });
 
   //card options
@@ -91,7 +123,7 @@ document.addEventListener("DOMContentLoaded", () => {
   cardArray.sort(() => 0.5 - Math.random());
 
   const grid = document.querySelector(".grid");
-  const resultDisplay = document.querySelector("#result");
+  const resultDisplay = document.querySelector(".text");
   let cardsChosen = [];
   let cardsChosenId = [];
   let cardsWon = [];
@@ -138,8 +170,7 @@ document.addEventListener("DOMContentLoaded", () => {
     resultDisplay.textContent = cardsWon.length;
     if (cardsWon.length === cardArray.length / 2) {
       text.style.display = "inline-block";
-      // resultDisplay.textContent = "Congratulations! You found them all!";
-      alert("asfdsa");
+      resultDisplay.textContent = "Congratulations! You found them all!";
     }
   }
 
